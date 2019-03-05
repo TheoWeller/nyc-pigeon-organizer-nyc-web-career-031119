b3c450104890905c8c946cbf050e740a708224ec
Theo Weller <weller.theodor@gmail.com>
2019-03-05 23:59:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |key, value|
+  value.map do |values, name|
+  name.each do |element|
+
+    pigeon_list[element] ||= {}
+    pigeon_list[element][key]||= []
+    pigeon_list[element][key] << values.to_s
+  end
+  end
+  end
+  pigeon_list
 end
